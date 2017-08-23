c76fdeef26556efde7687a8c0e074521225c6b2e
Charles MacKay <charlestmackay@gmail.com>
2017-08-22 22:28:44 -0700

    start from scratch to reorganize

--- scripts/detect_batch.sh
+++
@@ -1,1 +0,0 @@
-./darknet detector test_file data/obj.data data/yolo-obj.cfg weights/yolo-obj_30000.weights $1 -thresh $2 -outdir results
--- scripts/detect_batch_540.sh
+++
@@ -1,1 +0,0 @@
-./darknet detector test_file data/aic540.data data/aic540.cfg backup/aic540_final.weights $1 -thresh $2 -outdir results
